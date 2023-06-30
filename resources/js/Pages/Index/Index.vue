bdb60cd0fb631c300e3a09cb04ed83727cae40d6
Ilya Plotnikov <ivarthewebdeveloper@gmail.com>
2023-06-30 23:52:25 +0300

    reactive data in vue (refs)

--- resources/js/Pages/Index/Index.vue
+++ resources/js/Pages/Index/Index.vue
@@ -4,14 +4,18 @@
       <h1>Index page</h1>
       <Link href="/hello">Show page</Link>
       <div>The message is: {{ message }}</div>
+      <div>Timer is: {{ timer }}</div>
     </div>
   </MainLayout>
 </template>
 
 <script setup>
 import { Link } from "@inertiajs/vue3";
+import { ref } from "vue";
 import MainLayout from "../../Layouts/MainLayout.vue";
 defineProps({
   message: String,
 });
+const timer = ref(0);
+setInterval(() => timer.value++, 1000);
 </script>
