5c6e5662896abf386a2eb06b088b168ab079aafb
Ilya Plotnikov <ivarthewebdeveloper@gmail.com>
2023-06-30 23:54:11 +0300

    fix timer location

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -2,10 +2,14 @@
   <div>
     <Link href="/">Main Page</Link>
     <Link href="/hello">Show Page</Link>
+    <div>Timer is: {{ timer }}</div>
     <slot>Default</slot>
   </div>
 </template>
 
 <script setup>
 import { Link } from "@inertiajs/vue3";
+import { ref } from "vue";
+const timer = ref(0);
+setInterval(() => timer.value++, 1000);
 </script>
