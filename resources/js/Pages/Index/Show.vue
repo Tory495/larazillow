4e45919acb83464527b4688ae942b6ff9490dfec
Ilya Plotnikov <ivarthewebdeveloper@gmail.com>
2023-07-01 15:04:59 +0300

    set default layout

--- resources/js/Pages/Index/Show.vue
+++ resources/js/Pages/Index/Show.vue
@@ -8,10 +8,3 @@
 <script setup>
 import { Link } from "@inertiajs/vue3";
 </script>
-
-<script>
-import MainLayout from "../../Layouts/MainLayout.vue";
-export default {
-  layout: MainLayout,
-};
-</script>
