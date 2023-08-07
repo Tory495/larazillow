d1d4bc8b5c2592ad876400be6e3fa361127f53c9
Ilya Plotnikov <ivarthewebdeveloper@gmail.com>
2023-08-07 21:53:40 +0300

    mortgage calculator markup

--- resources/js/Pages/Listing/Show.vue
+++ resources/js/Pages/Listing/Show.vue
@@ -5,18 +5,35 @@
     </Box>
     <div class="md:col-span-5 flex flex-col gap-4">
       <Box>
-        <template #header>
-          Basic info
-        </template>
+        <template #header> Basic info </template>
         <Price :price="listing.price" class="text-2xl font-bold" />
         <ListingSpace :listing="listing" class="text-lg" />
         <ListingAddress :listing="listing" class="text-gray-500" />
       </Box>
       <Box>
-        <template #header>
-          Offer
-        </template>
-        Make an offer
+        <template #header> Monthly Payment </template>
+        <div>
+          <label class="label">Interest rate (2.5%)</label>
+          <input
+            class="w-full h-4 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700"
+            type="range"
+            min="0.1"
+            max="30"
+            step="0.1"
+          />
+          <label class="label">Duration (25 years)</label>
+          <input
+            type="range"
+            min="3"
+            max="35"
+            step="1"
+            class="w-full h-4 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700"
+          />
+          <div class="text-gray-600 dark:text-gray-300 mt-2">
+            <div class="text-gray-400">Your monthly payment</div>
+            <Price :price="500" class="text-3xl" />
+          </div>
+        </div>
       </Box>
     </div>
   </div>
