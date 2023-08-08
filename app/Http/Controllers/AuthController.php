8c6d0e535df18eb64ddc3af606fca4a6e5df9094
Ilya Plotnikov <ivarthewebdeveloper@gmail.com>
2023-08-08 23:59:30 +0300

    auth controller and routes

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function create()
+    {
+        
+    }
+
+    public function store(Request $request)
+    {
+
+    }
+
+    public function destroy()
+    {
+
+    }
+}
