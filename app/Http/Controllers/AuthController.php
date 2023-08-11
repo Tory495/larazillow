626b3c2fbd04551fe84d3bd66229166f63897b04
Tory495 <ivarthewebdeveloper@gmail.com>
2023-08-11 21:10:52 +0300

    signin in logic

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\ValidationException;
 
 class AuthController extends Controller
 {
@@ -13,11 +15,20 @@ class AuthController extends Controller
 
     public function store(Request $request)
     {
+        if (!Auth::attempt($request->validate([
+            'email' => 'required|string|email',
+            'password' => 'required|string',
+        ]), true)) {
+            throw ValidationException::withMessages([
+                'email' => 'Authentication failed'
+            ]);
+        };
 
+        $request->session()->regenerate();
+        return redirect()->intended();
     }
 
     public function destroy()
     {
-
     }
 }
