7ddea7ec81ad4545f85106b11058ca814efc7764
Ilya Plotnikov <ivarthewebdeveloper@gmail.com>
2023-08-13 19:31:05 +0300

    admin privileges

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,15 +14,22 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
         \App\Models\User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
-        
-        Listing::factory(20)->create([
+
+        \App\Models\User::factory()->create([
+            'name' => 'Ilya Plotnikov',
+            'email' => 'plotnikov.mtk@mail.ru',
+        ]);
+
+        Listing::factory(10)->create([
             'by_user_id' => 1
+        ]);
+
+        Listing::factory(10)->create([
+            'by_user_id' => 2
         ]);
     }
 }
